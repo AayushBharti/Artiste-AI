4bcdbb6461823568a9e0159663cbd065cf154f3b
Aayush Bharti <aayushbh2002@gmail.com>
2024-08-04 23:06:53 +0530

    created mobileNav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 import Sidebar from "@/components/Shared/Sidebar"
+import MobileNav from "../../components/Shared/MobileNav"
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <Sidebar />
-      {/* <MobileNav /> */}
+      <MobileNav />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
---
+++ components/Shared/MobileNav.tsx
@@ -0,0 +1,45 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { SignedIn, SignIn, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+
+const MobileNav = () => {
+  return (
+    <header className="header">
+      <Link href="/" className="flex items-center gap-2 md:py-2">
+        <Image
+          src="/assets/images/logo-text.svg"
+          alt="logo"
+          width={180}
+          height={28}
+        />
+      </Link>
+      <nav className="flex gap-2">
+        <SignedIn>
+          <UserButton />
+          <Sheet>
+            <SheetTrigger>Open</SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Are you absolutely sure?</SheetTitle>
+                <SheetDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </SignedIn>
+      </nav>
+    </header>
+  )
+}
+
+export default MobileNav
